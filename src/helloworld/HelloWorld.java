2abce292129864586338aec9ec87fcea640ddab5
bit <bit@DESKTOP-518UE4O>
2018-02-23 14:13:44 +0900

    .

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -1,9 +1,30 @@
 package helloworld;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World2");
+		Scanner s = new Scanner(System.in);
+		
+		int money = s.nextInt();
+		int result[] = new int[10];
+		boolean sw = true;
+		int flag = 50000;
+		for(int i = 0; i < 10; i++){
+			result[i] = money / flag;
+			money = money - (result[i] * flag);
+			if(sw == true){
+				System.out.println(flag + "원: " + result[i] + "개");
+				flag = flag / 5;
+				sw = false;
+			} else {
+				System.out.println(flag + "원: " + result[i] + "개");
+				flag = flag / 2;
+				sw = true;
+			}
+		}
+		
 
 	}
 
